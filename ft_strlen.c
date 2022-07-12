1a6db0295df1f87be6b4c5754332b2aaf479fe84
Amna Sajjad <asajjad@F2N005.42adel.org.au>
2022-07-12 10:08:16 +0930

    func 6 w header and norm

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,15 +1,25 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asajjad <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 09:58:00 by asajjad           #+#    #+#             */
+/*   Updated: 2022/07/12 10:07:50 by asajjad          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-size_t ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-    size_t length;
+	size_t	length;
 
-    length = 0;
-    while(*s++ != 0)
-    {
-        length++;
-    }
-    return(length);
+	length = 0;
+	while (*s++ != 0)
+	{
+		length++;
+	}
+	return (length);
 }
 /*
 int main()
