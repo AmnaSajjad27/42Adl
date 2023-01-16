37e9825d9d1062e841ba5e06e952d58ba400fe0a
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-01-16 17:03:22 +1030

    From 42 campus normed but shows error
    errors in the text file from franchinette

--- get_next_line.c
+++ get_next_line.c
@@ -6,119 +6,123 @@
 /*   By: asajjad <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/26 19:08:24 by asajjad           #+#    #+#             */
-/*   Updated: 2022/11/26 19:08:24 by asajjad          ###   ########.fr       */
+/*   Updated: 2023/01/12 18:32:28 by asajjad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 // free buffer and join using str_join
-char    *ft_free(char   *buffer, char   *buf)
+char	*ft_free(char *buffer, char *buf)
 {
-    char    tempo;
+	char	*tempo;
 
-    tempo = ft_str_join(buffer, buf);
-    free(buffer);
-    return (tempo);
+	tempo = ft_strjoin(buffer, buf);
+	free(buffer);
+	return (tempo);
 }
 
-// delete line find 
-char    *ft_next(char   *buffer)
+// delete line find
+// We find the length of the first line - line 34
+// If the end of the line is \0 then we return NULL - line 37
+// Allocate memory = length of file  length of first line + 1 - line 43
+// assigning line = buffer - storing it - line 47
+//
+char	*ft_next(char *buffer)
 {
-    int i;
-    int j;
-    char    *line;
+	int		i;
+	int		j;
+	char	*line;
 
-    i = 0;
-    // now we find the length of the first line
-    while (buffer[i] && buf[i] != '\n')
-    i++;
-    // if end of line is \0 then we return null
-    if (!buffer[i])
-    {
-        free(buffer);
-        return (NULL);
-    }
-    // allocate memory = length of file - lenght of first line + 1
-    line = ft_calloc((ft_str_len(buffer) - i + 1), sizeof(char));
-    i++;
-    j = 0;
-    // assigning line == buffer 
-    while (buffer[i])
-        line[j++] = buffer[i++];
-    return (line);
-    }
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	line = ft_calloc((ft_strlen(buffer) - i + 1), sizeof(char));
+	i++;
+	j = 0;
+	while (buffer[i])
+		line[j++] = buffer[i++];
+	free(buffer);
+	return (line);
+}
 
-    // function to return the line 
-    char    *ft_line(char   *buffer)
-    {
-        char    *line;
-        int     i;
-        
-        i = 0;
-        // we return null if bufffer is 0 i.e. no line to read
-        if (!buffer)
-            return (NULL);
-        // now we go to the end of line
-        while (buffer[i] && buffer[i] != '\n')
-            i++;
-        // now we malloc to the end of the line 
-        line = ft_calloc(i + 2, sizeof(char))
-        i = 0;
-        //line == buffer 
-        while (buffer[i] && buffer[i] != '\n')
-        {
-            line[i] = buffer[i];
-            i++;
-        }
-        // replace end of line with /n
-        if (buffer[i] && buffer[i] == '\n')
-            line[i++] = '\n';
-        return (line);
-        }
+// function to return the line
+// we return NULL if buffer is 0 i.e. there is no line to read 
+// while loop to go to the end of line 
+// then we malloc to the end of the line 
+// assign line = buffer
+// and lastly replace end of line with /n
+char	*ft_line(char *buffer)
+{
+	char	*line;
+	int		i;
 
-        // function to read file 
-        char    *read_file(int  fd, char    *res)
-        {
-            char    *buffer;
-            int     byte_rd;
+	i = 0;
+	if (!buffer)
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	line = ft_calloc(i + 2, sizeof(char));
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] && buffer[i] == '\n')
+		line[i++] = '\n';
+	return (line);
+}
 
-            // malloc if res doesnt exist 
-            if (!res)
-                res = ft_calloc(1,1);
-            // malloc buffer
-            buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-            byte_rd = 1;
-            while (byte_rd > 0)
-            {
-                // while not end of line 
-                byte_rd = read(fd, buffer, BUFFER_SIZE);
-                if (byte_rd == -1)
-                {
-                    free(buffer);
-                    return (NULL);
-                }
-                // zero to end so it dont leak
-                buffer[byte_rd] = 0;
-                // now we join and free the memory 
-                res = ft_free(res,buffer);
-                // we quit if we find \n
-                if (ft_strchr(buffer, '\n'))
-                break ;
-            }
-            free(buffer);
-            free(res);
-        }
+// function to read file
+// malloc if res doesnt exist
+// then malloc buffer
+// while loop to check whether its at the end of the line
+// buffer to 0 so it doesnt leak
+// now we join and free the memory 
+// if loop to quit if we find \n
+char	*read_file(int fd, char *res)
+{
+	char	*buffer;
+	int		byte_rd;
 
-// these two shouldnt be here
+	if (!res)
+		res = ft_calloc(1, 1);
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	byte_rd = 1;
+	while (byte_rd > 0)
+	{
+		byte_rd = read(fd, buffer, BUFFER_SIZE);
+		if (byte_rd == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[byte_rd] = 0;
+		res = ft_free(res, buffer);
+		if (ft_strchr(buffer, '\n'))
+		{
+			break ;
+		}
+	}
+	free(buffer);
+	return (res);
+}
 
-
+// error handling
+// If the file despcitor is not passed properly
+// there is no line to read i.e. empty file
+// if the static varibale buffer is not allocated properly
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
 	char		*line;
 
-	// error handling
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
 	buffer = read_file(fd, buffer);
